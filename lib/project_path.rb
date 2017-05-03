d6469b43760d5585cc0ce39c811f2ccdec749776
afeiship <1290657123@qq.com>
2017-05-03 11:22:07 +0800

    revert

--- lib/project_path.rb
+++ lib/project_path.rb
@@ -1,7 +1,13 @@
-# require "project_path/version"
+require "project_path/version"
 
 class ProjectPath
-  def self.hi
-    puts "Hello world!"
+  def self.discover_root
+    if Dir.glob('.git').length  == 1
+      Dir.pwd
+    else
+      Dir.chdir('..') do
+        self.discover_root
+      end
+    end
   end
 end
