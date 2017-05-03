6b455467b1fc3927fd43610bc2314a9fa67b890f
afeiship <1290657123@qq.com>
2017-05-03 11:19:31 +0800

    update

--- lib/project_path.rb
+++ lib/project_path.rb
@@ -1,13 +1,7 @@
-require "project_path/version"
+# require "project_path/version"
 
 class ProjectPath
-  def self.discover_root
-    if Dir.glob('.git').length  == 1
-      Dir.pwd
-    else
-      Dir.chdir('..') do
-        self.discover_root
-      end
-    end
+  def self.hi
+    puts "Hello world!"
   end
 end
